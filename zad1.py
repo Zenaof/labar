5e8614909eed5f5dbf748e167b5aee18df9a819d
zenaof <zenaof2610@gmail.com>
2024-02-08 21:46:01 +0300

    first commit

---
+++ zad1.py
@@ -0,0 +1,7 @@
+name = input("Скажите как вас звать: ")
+age = input("Скажите скока вам годиков: ")
+home = input("Скажите где вы живете: ")
+
+print(name)
+print(age)
+print(home)
---
+++ zad2.py
@@ -0,0 +1,11 @@
+import math
+
+#У меня 5 вариант вроде как если посчитать так что код вот такой
+
+a = 18
+b = 4
+p = 3.14
+
+c = abs(((math.sin(3 * a) + 2 * math.cos(3 * b)) / (5 * p * b - 2 * math.sqrt(a))) * math.e ** b)
+
+print(c)
